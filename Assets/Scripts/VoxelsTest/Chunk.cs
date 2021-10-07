7d180d1735e80c40b361cba4e36ab7c6a532ce48
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-10-07 19:22:22 +0200

    first test voxel complete

---
+++ Assets/Scripts/VoxelsTest/Chunk.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+
+public class Chunk : MonoBehaviour
+{
+    Block[,,] blocks;
+    public static int chunkSize = 16;
+    public bool update = true;
+
+    MeshFilter filter;
+    MeshCollider coll;
+
+    void Start()
+    {
+        filter = gameObject.GetComponent<MeshFilter>();
+        coll = gameObject.GetComponent<MeshCollider>();
+
+        // example chunk after this
+        blocks = new Block[chunkSize, chunkSize, chunkSize];
+
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    blocks[x, y, z] = new BlockAir();
+                }
+            }
+        }
+
+        blocks[3, 5, 2] = new Block();
+
+        UpdateChunk();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public Block GetBlock(int x, int y, int z)
+    {
+        return blocks[x, y, z];
+    }
+
+    // Updates the chunk based on its contents
+    void UpdateChunk()
+    {
+        MeshData meshData = new MeshData();
+
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    meshData = blocks[x, y, z].Blockdata(this, x, y, z, meshData);
+                }
+            }
+        }
+
+        RenderMesh(meshData);
+    }
+    // Sends the calculated mesh info to the mesh and collision comps
+    void RenderMesh(MeshData meshData)
+    {
+        filter.mesh.Clear();
+        filter.mesh.vertices = meshData.vertices.ToArray();
+        filter.mesh.triangles = meshData.triangles.ToArray();
+
+        filter.mesh.uv = meshData.uv.ToArray();
+        filter.mesh.RecalculateNormals();
+    }
+}
