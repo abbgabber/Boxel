5ce7081386f56cb9e7d46159c27b64bf858c9ab8
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-10-07 19:28:44 +0200

    Grass Blocks

---
+++ Assets/Scripts/Terrain/BlockTypes/BlockGrass.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGrass : Block
+{
+    public BlockGrass()
+        : base()
+    {
+
+    }
+
+    public override Tile TexturePosition(Direction direction)
+    {
+        Tile tile = new Tile();
+        /* FOR MULTITEXTURED BLOCKS
+        switch (direction)
+        {
+            case Direction.up:
+                tile.x = 2;
+                tile.y = 0;
+                return tile;
+            case Direction.down:
+                tile.x = 1;
+                tile.y = 0;
+                return tile;
+        }
+        */
+        tile.x = 1;
+        tile.y = 0;
+
+        return tile;
+    }
+}
