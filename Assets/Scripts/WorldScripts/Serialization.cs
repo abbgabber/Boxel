40029142a59ca89928427670508af4e96b5d458b
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-10-08 08:18:56 +0200

    test

---
+++ Assets/Scripts/WorldScripts/Serialization.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+public static class Serialization
+{
+    public static string saveFolderName = "boxelGameSaves";
+    
+    public static string SaveLocation(string worldName)
+    {
+        string saveLocation = saveFolderName + "/" + worldName + "/";
+        if (!Directory.Exists(saveLocation))
+        {
+            Directory.CreateDirectory(saveLocation);
+        }
+        return saveLocation;
+    }
+    public static string FileName(WorldPos chunkLocation)
+    {
+        string filename = chunkLocation.x + "," + chunkLocation.y + "," + chunkLocation.z + ".bin";
+        return filename;
+    }
+    public static void SaveChunk(Chunk chunk)
+    {
+        string saveFile = SaveLocation(chunk.world.worldName);
+        saveFile += FileName(chunk.pos);
+
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream(saveFile, FileMode.Create, FileAccess.Write, FileShare.None);
+        formatter.Serialize(stream, chunk.blocks);
+        stream.Close();
+    }
+    public static bool Load(Chunk chunk)
+    {
+        string saveFile = SaveLocation(chunk.world.worldName);
+        saveFile += FileName(chunk.pos);
+
+        if (!File.Exists(saveFile))
+            return false;
+
+        IFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(saveFile, FileMode.Open);
+
+        chunk.blocks = (Block[,,])formatter.Deserialize(stream);
+        stream.Close();
+        return true;
+    }
+}
