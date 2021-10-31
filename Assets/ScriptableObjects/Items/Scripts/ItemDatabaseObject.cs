8ebb216091be6f88028b30503d6198db74ade8dc
abbgabber <55151330+abbgabber@users.noreply.github.com>
2021-10-31 13:06:01 +0100

    Inventory system WIP

---
+++ Assets/ScriptableObjects/Items/Scripts/ItemDatabaseObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Item Database", menuName = "Inventory System/Items/Database")]
+public class ItemDatabaseObject : ScriptableObject, ISerializationCallbackReceiver
+{
+    public ItemObject[] items;
+    public Dictionary<int, ItemObject> GetItem = new Dictionary<int, ItemObject>();
+    public void OnAfterDeserialize()
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i].Id = i;
+            GetItem.Add(i, items[i]);
+        }
+    }
+    public void OnBeforeSerialize()
+    {
+        GetItem = new Dictionary<int, ItemObject>();
+    }
+}
