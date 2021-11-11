8c3d36e73d27873740ef0232ef9c5d7c7a28bf6d
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-11-11 15:52:42 +0100

    done for dayerino

--- Assets/Scripts/Menus/WorldSelect.cs
+++ Assets/Scripts/Menus/WorldSelect.cs
@@ -7,14 +7,18 @@ using System;
 
 public class WorldSelect : MonoBehaviour
 {
+    public GameObject worldButtonPreset;
+
     public void LoadGame()
     {
+        // Change Serialization.worldName to load in other world
+        //      gameObject.name;
         SceneManager.LoadScene("World");
     }
-    // Create meta file for each world which contains world name.
-    // For each folder in saves display name with a join button.
-    // When creating new world enter seed (?) and name.
+    // For each folder in saves display name with a join button
+    // When creating new world enter seed (?) and name
     // Multiplayer start thing will be from in game menu?
+
 
     public void ListWorld()
     {
@@ -25,6 +29,14 @@ public class WorldSelect : MonoBehaviour
             {
                 Debug.Log(dir);
             }
+            CreateButton();
         }
+    }
+
+    public void CreateButton()
+    {
+        GameObject button = Instantiate(worldButtonPreset, Vector3.zero, Quaternion.Euler(Vector3.zero)) as GameObject;
+        button.gameObject.SetActive(true);
+        // button.transform.GetChild(0).GetComponent<TextMesh>().text = "What";
     }
 }
