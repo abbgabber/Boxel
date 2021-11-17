2dbad5f3803d682ad9e2c045b389c81e3ff32346
abbliseng <54662679+abbliseng@users.noreply.github.com>
2021-11-17 16:40:05 +0100

    crosshair fix + water serializable

--- Assets/Scripts/Menus/WorldSelect.cs
+++ Assets/Scripts/Menus/WorldSelect.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
 using System;
+using TMPro;
 
 public class WorldSelect : MonoBehaviour
 {
     public GameObject worldButtonPreset;
+    public TextMeshProUGUI txtMesh;
+    public GameObject Overlord;
 
     public void LoadGame()
     {
@@ -24,10 +27,16 @@ public class WorldSelect : MonoBehaviour
     {
         if (Directory.Exists(Application.persistentDataPath + '/' + "saves"))
         {
+            int startY = 250;
             string[] dirs = Directory.GetDirectories(Application.persistentDataPath + '/' + "saves");
             foreach (string dir in dirs)
             {
-                Debug.Log(dir);
+                String name = dir.Split('/')[dir.Split('/').Length-1].Split('\\')[1];
+                GameObject button = Instantiate(worldButtonPreset, Vector3.zero, Quaternion.Euler(Vector3.zero), Overlord.transform);
+                button.SetActive(true);
+                button.GetComponent<RectTransform>().localPosition = new Vector3(0, startY, 0); // First one
+                button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = name;
+                startY -= 35;
             }
             CreateButton();
         }
@@ -35,8 +44,7 @@ public class WorldSelect : MonoBehaviour
 
     public void CreateButton()
     {
-        GameObject button = Instantiate(worldButtonPreset, Vector3.zero, Quaternion.Euler(Vector3.zero)) as GameObject;
-        button.gameObject.SetActive(true);
+        
         // button.transform.GetChild(0).GetComponent<TextMesh>().text = "What";
     }
 }
